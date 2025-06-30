c39c8cb94b0e2526a9c633a5838e67f4b58352dd
sameerpanhwarit <samar034836@gmail.com>
2025-06-30 23:19:02 +0500

    fixed undetected emotions issue

--- model_loader.py
+++ model_loader.py
@@ -5,6 +5,7 @@ from PIL import Image
 model = load_model("./Model/FER_DATA.keras")
 
 emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+map_emotion = {'angry':'metal','disgust':'punk','fear':'electronic','happy':'pop','sad':'acoustic','surprise':'edm','neutral':'classical'}
 
 def predict_emotion(image_path: str) -> str:
     img = Image.open(image_path).convert('L').resize((48, 48))
@@ -13,4 +14,5 @@ def predict_emotion(image_path: str) -> str:
 
     predictions = model.predict(img_array)
     emotion_index = np.argmax(predictions)
-    return emotion_labels[emotion_index]
+    emotion = emotion_labels[emotion_index]
+    return map_emotion[emotion]
